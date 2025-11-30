67311da4847698f6a23a1e5a30eefcb63f6851c8
nhat251 <nsn251tp@gmail.com>
2025-11-30 21:07:38 +0700

    temp learning page

--- src/pages/MyLearning/index.jsx
+++ src/pages/MyLearning/index.jsx
@@ -1,28 +1,20 @@
-import React, { useEffect, useState } from 'react';
-import callApi from '~/api/axiosConfig';
-import { GET_TOP_USER_ENDPOINT } from '~/constants/my_const';
-import { useAuth } from '~/hooks';
+import classNames from 'classnames/bind';
+import styles from './MyLearning.module.scss';
 
+import React from 'react';
+
+import { ContinueList, Leaderboard } from '~/layouts/components';
+
+const cx = classNames.bind(styles);
 function MyLearning() {
-  const { user } = useAuth();
-
-  const [list, setList] = useState([]);
-  useEffect(() => {
-    const init = async () => {
-      const response = await callApi({ path: GET_TOP_USER_ENDPOINT, params: { page: 1, size: 3 } });
-      const paged = response.result;
-
-      setList(paged.items);
-    };
-    init();
-  }, []);
   return (
     <>
-      <div>MyLearning</div>
-      <p>ADMIN: {user.userName}</p>
-      {list.map((u, index) => (
-        <p key={index}>{u.userName}</p>
-      ))}
+      <div className={cx('wrapper')}>
+        <div className={cx('container')}>
+          <Leaderboard />
+          <ContinueList title="Tiếp tục làm" />
+        </div>
+      </div>
     </>
   );
 }
